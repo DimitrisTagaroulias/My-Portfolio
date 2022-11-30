7ca0b8211394d1bf8494925b1eeb070f68f0aac8
dimitris <dimitristagaroulias@yahoo.gr>
2022-11-30 12:53:08 +0200

    Services Completed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Particles from "./components/Particles";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
+      <Particles />
       <Navbar />
       <Header />
-      <AboutMe />
-      <Particles />
+      <div className="my-info">
+        <AboutMe />
+        <Services />
+      </div>
     </>
   );
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,20 +3,22 @@ import author from "../images/Me_400px.jpg";
 
 const AboutMe = () => {
   return (
-    <div className="container py-5">
-      <div className="row">
-        <div className="col-lg-6 col-xm-12">
-          <div className="photo-wrap mb-5">
-            <img className="profile-img" src={author} alt="author..."></img>
+    <div className="about-me-container">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-lg-6 col-xm-12">
+            <div className="photo-wrap mb-5">
+              <img className="profile-img" src={author} alt="author..."></img>
+            </div>
           </div>
-        </div>
-        <div className="col-lg-6 col-xm-12">
-          <h1 className="about-heading">about me</h1>
-          <p>
-            Hello! I am Dimitris.
-            <br />
-            ...blah blah blah...
-          </p>
+          <div className="col-lg-6 col-xm-12">
+            <h1 className="about-heading">about me</h1>
+            <p>
+              Hello! I am Dimitris.
+              <br />
+              ...blah blah blah...
+            </p>
+          </div>
         </div>
       </div>
     </div>
