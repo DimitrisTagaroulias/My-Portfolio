248b1c74268e4750040fe11c8fa5e2a7eb1c6394
dimitris <dimitristagaroulias@yahoo.gr>
2022-11-30 14:40:15 +0200

    Experience component completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <div className="my-info">
         <AboutMe />
         <Services />
+        <Experience />
       </div>
     </>
   );
