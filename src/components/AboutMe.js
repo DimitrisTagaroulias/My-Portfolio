12261e67b55a7007ebdb1ee70b1ac0e82ffed972
dimitris <dimitristagaroulias@yahoo.gr>
2023-03-13 22:14:50 +0200

    about me completed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,9 +14,24 @@ const AboutMe = () => {
           <div className="about-me-container col-lg-6 col-xm-12">
             <h1 className="about-heading">about me</h1>
             <p>
-              Hello! I am Dimitris.
-              <br />
-              ...blah blah blah...
+              My name is <b>Dimitris Tagaroulias</b> and I'm 36 years old.
+              <br></br>
+              <br></br>
+              Recently, I have completed an educational and training program on{" "}
+              <b>Front-End Web Developer</b> field in{" "}
+              <b>Ethnikon and Kapodistriakon Panepistimio Athinon (E.K.P.A)</b>{" "}
+              and I have been certified by passing the exams. I have also got a
+              certification on <b>Full-Stack Web Developer</b> field by
+              attending courses, passing the exams and delivering a project to{" "}
+              <b>CollegeLink</b>.<br></br>
+              <br></br>
+              Apart from the above training programs I have{" "}
+              <b>studied a numerous online courses</b> through Udemy and other
+              similar platforms and <b>created my own projects</b> in order to
+              practice and enrich my portfolio. As I m fascinated by the
+              Front-End Web Development field, I would like to have the
+              opportunity to be part of a company, where I can improve my skills
+              and have a long term cooperation.
             </p>
           </div>
         </div>
